b6d2910d39b55c2e87439d89011850d4340d08b8
harinivaskumar <harinivaskumar@live.com>
2016-08-15 04:42:03 +0530

    Adding support for Volley Library

---
+++ app/src/main/java/com/hari/aund/travelbuddy/app/TravelBuddyApp.java
@@ -0,0 +1,56 @@
+package com.hari.aund.travelbuddy.app;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Hari Nivas Kumar R P on 8/15/2016.
+ */
+public class TravelBuddyApp extends Application {
+
+    public static final String TAG = TravelBuddyApp.class.getSimpleName();
+    private static TravelBuddyApp mInstance;
+    private RequestQueue mRequestQueue;
+
+    public TravelBuddyApp() {
+        super();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized TravelBuddyApp getInstance() {
+        return mInstance;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
