0f6aef4b38be3a955f65142fa2660debf0893e0b
harinivaskumar <harinivaskumar@live.com>
2016-08-24 03:28:12 +0530

    FavouritesFargment - Implementation - Step 2

--- app/src/main/java/com/hari/aund/travelbuddy/fragment/FavouritesFragment.java
+++ app/src/main/java/com/hari/aund/travelbuddy/fragment/FavouritesFragment.java
@@ -1,20 +1,26 @@
 package com.hari.aund.travelbuddy.fragment;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.hari.aund.travelbuddy.R;
 import com.hari.aund.travelbuddy.activity.MainActivity;
+import com.hari.aund.travelbuddy.data.PlacesCategoryValues;
 import com.hari.aund.travelbuddy.utils.DefaultValues;
 import com.hari.aund.travelbuddy.utils.Utility;
+import com.pnikosis.materialishprogress.ProgressWheel;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -25,11 +31,21 @@ public class FavouritesFragment extends Fragment
     private static final String LOG_TAG = FavouritesFragment.class.getSimpleName();
 
     private static final int PREFERENCE_MODE_PRIVATE = 0;
+    private static final int FILTER_ALL_PLACES = 1;
+    private static final int FILTER_CATEGORY = 2;
+    private static final int FILTER_SUB_TYPE = 3;
+    private static final String ALL_PLACES_TITLE = "All Places";
 
     private int mNavSectionId;
+    private int mSelectedFilterId = FILTER_ALL_PLACES;
     private String mNavSectionName;
+    private String mSelectedCategoryName;
+    private String mSelectedSubTypeName;
     private ActionBar mActionBar = null;
     private SharedPreferences mSharedPreferences;
+    private ProgressWheel mProgressWheel;
+    private FloatingActionMenu fabMenu;
+    private FloatingActionButton allPlacesFab, categoryFab, subTypeFab;
 
     public FavouritesFragment() {
     }
@@ -51,12 +67,23 @@ public class FavouritesFragment extends Fragment
 
         mSharedPreferences = getActivity().getPreferences(PREFERENCE_MODE_PRIVATE);
         readValues();
-        setFragmentTitle();
+        setFragmentTitle(FILTER_ALL_PLACES, ALL_PLACES_TITLE);
 
         View rootView = inflater.inflate(R.layout.fragment_favourites, container, false);
 
-        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab_favourites);
-        fab.setOnClickListener(this);
+        mProgressWheel = (ProgressWheel) rootView.findViewById(R.id.progress_wheel_favourites);
+        mProgressWheel.spin();
+
+        fabMenu = (FloatingActionMenu) rootView.findViewById(R.id.fab_favourites_menu);
+        allPlacesFab = (FloatingActionButton) rootView.findViewById(R.id.fab_favourites_all_places);
+        categoryFab = (FloatingActionButton) rootView.findViewById(R.id.fab_favourites_category);
+        subTypeFab = (FloatingActionButton) rootView.findViewById(R.id.fab_favourites_sub_type);
+
+        fabMenu.setOnClickListener(this);
+        fabMenu.close(true);
+        allPlacesFab.setOnClickListener(this);
+        categoryFab.setOnClickListener(this);
+        subTypeFab.setOnClickListener(this);
 
         return rootView;
     }
@@ -88,9 +115,18 @@ public class FavouritesFragment extends Fragment
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.fab_favourites:
-                Snackbar.make(view, "Use this to filter your Favourites!", Snackbar.LENGTH_LONG)
-                        .show();
+            case R.id.fab_favourites_all_places:
+                setLocalValuesAndUpdateUI(FILTER_ALL_PLACES, ALL_PLACES_TITLE);
+                break;
+            case R.id.fab_favourites_category:
+                if (!displayCategorySelectorList())
+                    Toast.makeText(getContext(), "No Category Present", Toast.LENGTH_SHORT)
+                            .show();
+                break;
+            case R.id.fab_favourites_sub_type:
+                if (!displaySubTypeSelectorList())
+                    Toast.makeText(getContext(), "No Sub Type Present", Toast.LENGTH_SHORT)
+                            .show();
                 break;
         }
     }
@@ -114,6 +150,136 @@ public class FavouritesFragment extends Fragment
         }
     }
 
+    private boolean displayCategorySelectorList() {
+        //TODO: fetch data from Database & display it.
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                getContext(), android.R.layout.select_dialog_singlechoice);
+
+        for (int index = 0; index < PlacesCategoryValues.MAX_PLACES_CATEGORIES; index++)
+            arrayAdapter.add(PlacesCategoryValues.placesCategories[index]);
+
+        String defaultCategoryStr = (mSelectedCategoryName == null ?
+                PlacesCategoryValues.placesCategories[0] : mSelectedCategoryName);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Select a Category as Filter");
+        builder.setNegativeButton("cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }
+        );
+        builder.setSingleChoiceItems(arrayAdapter,
+                arrayAdapter.getPosition(defaultCategoryStr),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setLocalValuesAndUpdateUI(FILTER_CATEGORY,
+                                arrayAdapter.getItem(which));
+                        dialog.dismiss();
+                    }
+                }
+        );
+        builder.show();
+        return true;
+    }
+
+    private boolean displaySubTypeSelectorList() {
+        //TODO: fetch data from Database & display it.
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                getContext(), android.R.layout.select_dialog_singlechoice);
+
+        arrayAdapter.add(PlacesCategoryValues.bankingSubTypes[0]);
+        arrayAdapter.add(PlacesCategoryValues.commuteSubTypes[0]);
+        arrayAdapter.add(PlacesCategoryValues.emergencySubTypes[0]);
+        arrayAdapter.add(PlacesCategoryValues.hangoutSubTypes[0]);
+        arrayAdapter.add(PlacesCategoryValues.healthCareSubTypes[0]);
+        arrayAdapter.add(PlacesCategoryValues.personalCareSubTypes[0]);
+        arrayAdapter.add(PlacesCategoryValues.restaurantSubTypes[0]);
+        arrayAdapter.add(PlacesCategoryValues.shoppingSubTypes[0]);
+        arrayAdapter.add(PlacesCategoryValues.templeSubTypes[0]);
+        arrayAdapter.add(PlacesCategoryValues.touristsSpotSubTypes[0]);
+
+        String defaultSubTypeStr = (mSelectedSubTypeName == null ?
+                PlacesCategoryValues.bankingSubTypes[0] : mSelectedSubTypeName);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Select a Sub Type as Filter");
+        builder.setNegativeButton("cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }
+        );
+        builder.setSingleChoiceItems(arrayAdapter,
+                arrayAdapter.getPosition(defaultSubTypeStr),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setLocalValuesAndUpdateUI(FILTER_SUB_TYPE,
+                                arrayAdapter.getItem(which));
+                        dialog.dismiss();
+                    }
+                }
+        );
+        builder.show();
+        return true;
+    }
+
+    private void setLocalValuesAndUpdateUI(int filterId, String titleStr){
+        setFragmentTitle(filterId, titleStr);
+        setFabImageResource();
+
+        fabMenu.close(true);
+        mProgressWheel.stopSpinning();
+    }
+
+    private void setFragmentTitle(int filterId, String titleStr) {
+        String titleStrPrefix = "Favourites - ";
+        mSelectedFilterId = filterId;
+        if (mActionBar != null &&
+                titleStr != null && !titleStr.isEmpty()) {
+            switch (mSelectedFilterId) {
+                case FILTER_ALL_PLACES:
+                    break;
+                case FILTER_CATEGORY:
+                    mSelectedCategoryName = titleStr;
+                    break;
+                case FILTER_SUB_TYPE:
+                    mSelectedSubTypeName = titleStr;
+                    break;
+            }
+            titleStrPrefix += titleStr;
+            mActionBar.setTitle(titleStrPrefix);
+        }
+    }
+
+    private void setFabImageResource() {
+        int enabledDrawableId = R.drawable.ic_filter_list_black_24dp;
+        int disabledDrawableId = R.drawable.ic_filter_list_white_24dp;
+        switch (mSelectedFilterId) {
+            case FILTER_ALL_PLACES:
+                allPlacesFab.setImageResource(enabledDrawableId);
+                categoryFab.setImageResource(disabledDrawableId);
+                subTypeFab.setImageResource(disabledDrawableId);
+                break;
+            case FILTER_CATEGORY:
+                allPlacesFab.setImageResource(disabledDrawableId);
+                categoryFab.setImageResource(enabledDrawableId);
+                subTypeFab.setImageResource(disabledDrawableId);
+                break;
+            case FILTER_SUB_TYPE:
+                allPlacesFab.setImageResource(disabledDrawableId);
+                categoryFab.setImageResource(disabledDrawableId);
+                subTypeFab.setImageResource(enabledDrawableId);
+                break;
+        }
+    }
+
     private void setNavSectionId(int navSectionId) {
         this.mNavSectionId = navSectionId;
     }
@@ -128,11 +294,5 @@ public class FavouritesFragment extends Fragment
 
     private String getNavSectionName() {
         return mNavSectionName;
-    }
-
-    private void setFragmentTitle() {
-        if (mActionBar != null) {
-            mActionBar.setTitle(getNavSectionName());
-        }
     }
 }
