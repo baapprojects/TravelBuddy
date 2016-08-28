967bf12a3d06cf6844744bcb5b01ba0b9dd9cb98
harinivaskumar <harinivaskumar@live.com>
2016-08-28 18:12:24 +0530

    Cleaning - unused menu settings

--- app/src/main/java/com/hari/aund/travelbuddy/activity/PlacesSubTypeActivity.java
+++ app/src/main/java/com/hari/aund/travelbuddy/activity/PlacesSubTypeActivity.java
@@ -10,8 +10,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
@@ -123,28 +121,6 @@ public class PlacesSubTypeActivity extends AppCompatActivity
         mSaveToPreference = false;
         overridePendingTransition(
                 R.animator.activity_open_translate, R.animator.activity_close_translate);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_places_category, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
